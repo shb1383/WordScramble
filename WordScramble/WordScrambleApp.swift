56a2205bcb0b9a836c6b37810b3cc0df9b7a5abe
syrene <144994323+shb1383@users.noreply.github.com>
2024-10-02 20:47:20 +0900

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Syrene Haber Bartolome on 2024/10/02.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WordScrambleTests/WordScrambleTests.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleTests.swift
+//  WordScrambleTests
+//
+//  Created by Syrene Haber Bartolome on 2024/10/02.
+//
+
+import Testing
+@testable import WordScramble
+
+struct WordScrambleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
