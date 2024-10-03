f1559aecdf439913e010da373b97d45f5290129c
syrene <144994323+shb1383@users.noreply.github.com>
2024-10-03 21:29:56 +0900

    practiced strings

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -18,12 +18,14 @@ struct ContentView: View {
         .padding()
     }
     
-    func testBundles() {
-        if let fileURL = Bundle.main.url(forResource: "somefile", withExtension: "txt") {
-            if let fileContents = try? String(contentsOf: fileURL) {
-                // we loaded the file into a string!
-            }
-        }
+    func testStrings() {
+        let word = "swift"
+        let checker = UITextChecker()
+        
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        let allGood = misspelledRange.location == NSNotFound
     }
 }
 
